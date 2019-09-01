b84b89417547b46b9faf1e0e409c4b6e99ebec6f
github username <github email address>
2019-08-31 19:39:36 -0500

    auth player

--- src/components/Player.js
+++ src/components/Player.js
@@ -12,12 +12,12 @@ const Player = props => {
 
   return (
     <div>
-      <div className="main-wrapper">
-        <div className="now-playing__img">
-          <img src={props.item.album.images[0].url} />
-        </div>
-        <div className="now-playing__side">
-          <div className="now-playing__name">{props.item.name}</div>
+      <div className="now-playing__img">
+        <img src={props.item.album.images[0].url} />
+      </div>
+      <div className="now-playing__side">
+          <div className="now-playing__name">{props.item.name}
+          </div>
           <div className="now-playing__artist">
             {props.item.artists[0].name}
           </div>
@@ -25,13 +25,10 @@ const Player = props => {
             {props.is_playing ? "Playing" : "Paused"}
           </div>
           <div className="progress">
-            <div
-              className="progress__bar"
-              style={progressBarStyles}
-            />
-          </div>
-        </div>
-        <div className="background" style={backgroundStyles} />{" "}
+          <div className="progress__bar" style={progressBarStyles}/>
+      </div>
+      <div className="background" style={backgroundStyles} />
+        {" "}
       </div>
     </div>
   );
